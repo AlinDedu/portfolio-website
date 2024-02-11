5578c52ed6628ec5a6cdedaeaee2c93594d81730
Alin Dedu <alin.dedu93@gmail.com>
2024-02-11 23:59:39 +0200

    Updated about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -8,7 +8,7 @@ const TAB_DATA = [
 		title: 'Skills',
 		id: 'skills',
 		content: (
-			<ul className='list-disc pt-2'>
+			<ul className='list-disc pl-2'>
 				<li>JavaScript</li>
 				<li>Java</li>
 				<li>React</li>
@@ -22,7 +22,7 @@ const TAB_DATA = [
 		title: 'Education',
 		id: 'education',
 		content: (
-			<ul className='list-disc pt-2'>
+			<ul className='list-disc pl-2'>
 				<li>Full Stack Developer - CodeCool, Romania</li>
 				<li>Liceul Victor Frunza, Ramnicu Sarat, Buzau</li>
 			</ul>
@@ -32,7 +32,7 @@ const TAB_DATA = [
 		title: 'Experience',
 		id: 'experience',
 		content: (
-			<ul className='list-disc pt-2'>
+			<ul className='list-disc pl-2'>
 				<li>SGP MApN, Cristian, Brasov</li>
 			</ul>
 		),
@@ -53,14 +53,19 @@ const AboutSection = () => {
 
 	return (
 		<section id='about' className='text-white'>
-			<div className='flex flex-col md:flex-row gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-				<div className='flex items-center md:w-1/2'>
+			<div className='flex flex-col xl:flex-row gap-8 items-center pt-8 px-4 xl:gap-16 sm:pt-16 xl:px-16'>
+				<div className='flex items-center xl:w-1/2'>
 					<Image src='/images/about-image.png' alt='About Image' width={500} height={500} />
 				</div>
-				<div className='mt-4 md:mt-0 text-left flex flex-col w-full md:w-1/2'>
-					<h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
-					<p className='text-base lg:text-lg'>{/* ... (your existing text content) */}</p>
-					<div className='flex flex-row justify-start mt-8 '>
+				<div className='mt-4 xl:mt-0 text-left flex flex-col w-full xl:w-1/2'>
+					<h2 className='text-4xl font-bold text-white mb-4 text-center'>About Me</h2>
+					<p className='text-base lg:text-lg'>
+						I am a full stack web developer with a passion for creating interactive and responsive web applications. I
+						have experience working with JavaScript, Java, React, Redux, Node.js, Express, PostgreSQL, HTML, CSS, and
+						Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player
+						and I am excited to work with others to create amazing applications.
+					</p>
+					<div className='flex flex-row justify-start mt-8'>
 						<TabButton selectTab={() => handleTabChange('skills')} active={tab === 'skills'}>
 							Skills
 						</TabButton>
@@ -71,7 +76,7 @@ const AboutSection = () => {
 							Experience
 						</TabButton>
 					</div>
-					<div className='mt-8' style={{ height: '300px', overflowY: 'auto' }}>
+					<div className='mt-8' style={{ height: '300px' }}>
 						{tabContent}
 					</div>
 				</div>
